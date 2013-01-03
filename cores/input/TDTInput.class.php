385a0f39e570965a5da6060c7d80e66dc8495017
Pieter Colpaert <pieter@iRail.be>
2013-01-03 17:18:24 +0100

    added first controller

---
+++ cores/input/TDTInput.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class TDTInput {
+
+    private $e,$t,$m,$l;
+    
+    public function __construct($e_conf, $t_conf,$m_conf,$l_conf){
+        
+    }
+    
+    public function execute($source){
+        
+    }
+    
+
+}
--- cores/input/controllers/DataController.class.php
+++ cores/input/controllers/DataController.class.php
@@ -1,5 +1,27 @@
 <?php
-
+/**
+ * The Datacontroller retrieves requests to reload data in the datastore
+ *
+ */
 class DataController extends AController {
+
+    public function GET($matches){
+        if(parent::isBasicAuthenticated()){
+            $input = parse_ini_file("../custom/input.ini", true);
+            //check if resource exists
+            if(isset($input[$matches[0]])){
+                $input = $input[$matches[0]];
+//                ob_end_flush();
+                var_dump($input);
+//                ob_start();
+//                ignore_user_abort(true);
+//                set_time_limit(0);
+            }else{
+                throw new TDTException(404,array("data/" . $matches[0]));
+            }
+        }else{
+            throw new TDTException(401,array());
+        }
+    }
 
 }
