2c2726bdcaac186e05a83437b71e9a976a70d937
Miel Vander Sande <miel.vandersande@ugent.be>
2013-01-10 11:43:24 +0100

    Finished mapping & loading

--- cores/input/model/load/RDFLoader.class.php
+++ cores/input/model/load/RDFLoader.class.php
@@ -2,32 +2,81 @@
 
 class RDFLoader extends ALoader {
 
-    private $file = "output.ttl";
-    private $conn;
+    private $endpoint = 'http://157.193.213.125:8890/sparql';
+    private $format = 'json';
+    private $graph = 'http://mytest.com/regions';
     
-    public function __construct($config){
+    private $buffer_size = 1; //amount of chunks that are being inserted into one request
+    private $buffer_index = 0;
+    private $buffer = "";
+
+    public function __construct($config) {
         parent::__construct($config);
-        
-        $this->conn = odbc_connect('VOS', 'dba', 'dba');
-        echo 'ERROR: ' . odbc_errormsg();
     }
 
-        public function execute(&$chunk) {
-        //$gs = new EasyRdf_GraphStore('http://localhost:2222/update/');
-        //$gs->insert($chunk);
+    public function execute(&$chunk) {
+        $start = microtime(true);
+
         if (!$chunk->is_empty()) {
-            $stringData = $chunk->to_rdfxml();
-
-//            $fh = fopen($this->file, 'w') or die("can't open file");
-//            fwrite($fh, $stringData);
-//            fclose($fh);
-            //echo $stringData;
+            $this->buffer .= $chunk->to_ntriples();
+            $this->buffer_index += 1;
             
-            //$result = odbc_exec($conn, 'CALL DB.DBA.SPARQL_EVAL(\'' . $query . '\', NULL, 0)');
-            $result = odbc_exec($this->conn, 'CALL DB.DBA.RDF_LOAD_RDFXML_MT(\'' . $stringData . '\',\'\', \'http://mytest.com\')');
-
-            echo 'Triple loaded';
+            if ($this->buffer_size <= $this->buffer_index) {
+                $this->query($this->buffer);
+                $this->buffer = "";
+                $this->buffer_index = 0;
+            }
         }
+
+        $duration = microtime(true) - $start;
+        echo "->Loading executed in $duration ms - buffer $this->buffer_index/$this->buffer_size \n";
+    }
+
+    private function query($triples) {
+        $query = "INSERT IN GRAPH <$this->graph> { ";
+        $query .= $triples;
+        $query .= ' }';
+
+        $response = json_decode($this->execSPARQL($query), true);
+
+        if (!$response)
+            $msg = "Query not inserted: $query";
+        else
+            $msg = $response['results']['bindings'][0]['callret-0']['value'] . '\n';
+        
+        echo $msg;
+        
+        Log::getInstance()->logInfo($msg);
+    }
+
+    private function execSPARQL($query) {
+
+        // is curl installed?
+        if (!function_exists('curl_init')) {
+            die('CURL is not installed!');
+        }
+
+        // get curl handle
+        $ch = curl_init();
+
+        // set request url
+        curl_setopt($ch, CURLOPT_URL, $this->endpoint
+                . '?query=' . urlencode($query)
+                . '&format=' . $this->format);
+
+        // return response, don't print/echo
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        /*
+          Here you find more options for curl:
+          http://www.php.net/curl_setopt
+         */
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $response;
     }
 
 }
--- cores/input/model/map/RDFMapper.class.php
+++ cores/input/model/map/RDFMapper.class.php
@@ -23,7 +23,7 @@ class RDFMapper extends AMapper {
     function __construct($config) {
         parent::__construct($config);
         
-        $spec_file_name = 'http://localhost:8888/airports.csv.spec.ttl';
+        $spec_file_name = 'http://localhost:8888/regions.csv.spec.ttl';
         $spec_file = file_get_contents($spec_file_name);
         $spec = new SimpleGraph();
         $spec->from_turtle($spec_file);
@@ -33,24 +33,20 @@ class RDFMapper extends AMapper {
         if (count($specs) != 1) {
             throw new Exception('spec document must contain exactly one conversion spec');
         }
-
+        
+        //Check if mapping file is the current one
+        //Load spec and create new Vertere converter
         $this->vertere = new Vertere($spec, $specs[0]);
     }
 
     public function execute(&$chunk) {
-//        $graph = new EasyRdf_Graph();
-//        $baseurl = Config::get("general","hostname").Config::get("general","subdir");
-//        
-//        $thing = $graph->resource($baseurl . $chunk["0"], $this->config["row"]["class"]);
-//        $i = 0;
-//        foreach($this->config["columns"] as $index => $col){
-//            $thing->set($col,$chunk[$index]);
-//            $i++;
-//        }
-//        return $graph;
-        //Check if mapping file is the current one
-        //Load spec and create new Vertere converter
-        return $this->vertere->convert_array_to_graph($chunk);
+        $start = microtime(true);
+        $graph = $this->vertere->convert_array_to_graph($chunk);
+        
+        $duration = microtime(true) - $start;
+        echo "->Mapping executed in $duration ms\n";
+        
+        return $graph;
     }
 
 }
