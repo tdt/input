5bf478ed149180c46d5e7acd74d2c7e3b07ab3b0
Pieter Colpaert <pieter@iRail.be>
2013-01-25 18:23:25 +0100

    added first scheduler stuff

---
+++ src/tdt/input/scheduler/Worker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tdt/input/scheduler;
+
+class Worker extends AController{
+    /**
+     * @param $config is an array with db config
+     */
+    public function __construct(array $config){
+        
+
+    }
+}
