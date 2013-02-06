edeb675e214232a45014d3853d06a3299e48cb3b
Pieter Colpaert <pieter@iRail.be>
2013-02-06 11:41:38 +0100

    sync

--- src/tdt/input/scheduler/Worker.php
+++ src/tdt/input/scheduler/Worker.php
@@ -2,12 +2,16 @@
 
 namespace tdt/input/scheduler;
 
+/**
+ * When using tdt/start, you can use this class to map it to a route
+ *
+ */
 class Worker extends AController{
     /**
      * @param $config is an array with db config
      */
     public function __construct(array $config){
         
-
     }
 }
+
