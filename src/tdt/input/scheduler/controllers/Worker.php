15b6c06aaa81422bdc01d87a613d67e4a5d5d196
Pieter Colpaert <pieterc@member.fsf.org>
2013-02-20 21:18:47 +0100

    added a Worker

---
+++ src/tdt/input/scheduler/controllers/Worker.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace tdt\input\scheduler;
+
+class Worker{
+    /**
+     * Only works with tdt/start
+     */
+    private function getDBConfig(){
+        $db = array();
+        $db["host"] = Config::get("db", "host");
+        $db["name"] = Config::get("db", "name");
+        $db["system"] = Config::get("db", "system");
+        $db["user"] = Config::get("db", "user");
+        $db["password"] = Config::get("db", "password");
+        return $db;
+    }
+
+
+    public function GET($matches){
+        if($this->isBasicAuthenticated()){
+            ignore_user_abort(true);
+            set_time_limit(0);
+            \tdt\core\utility\Config::setConfig(Config::getConfigArray());
+            $s = new Schedule($this->getDBConfig());
+            $s->execute();
+        }else{
+            header('WWW-Authenticate: Basic realm="' . $this->hostname . $this->subdir . '"');
+            header('HTTP/1.0 401 Unauthorized');
+            exit();
+        }
+        
+    }
+}
