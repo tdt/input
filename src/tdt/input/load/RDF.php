e2eecbf5b3c696cffedc5d6361dffd06f73a9014
Miel Vander Sande <miel.vandersande@ugent.be>
2013-04-29 10:31:18 +0200

    Clear graphs enabled

--- src/tdt/input/load/RDF.php
+++ src/tdt/input/load/RDF.php
@@ -140,7 +140,7 @@ class RDF extends \tdt\input\ALoader {
             echo "Request to add resource in The DataTank succeeded with code " . $response_code . " and message \"$response\"\n";
             echo "Resources available under " . $this->datatank_uri . "$this->datatank_package/$this->datatank_resource\n";
         }
-        //$this->clearOldGraphs();
+        $this->clearOldGraphs();
     }
 
     public function execute(&$chunk) {
