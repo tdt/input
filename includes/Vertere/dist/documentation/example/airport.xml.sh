55c6cb18ee97e836df5fde41468e7a312109b29a
andimou <Anastasia.Dimou@ugent.be>
2013-07-17 08:16:16 +0200

    Update airport.xml.sh

--- includes/Vertere/dist/documentation/example/airport.xml.sh
+++ includes/Vertere/dist/documentation/example/airport.xml.sh
@@ -1,4 +1,4 @@
-curl http://localhost/test/public/tdtinput/airports/airports_xml -XPUT -d '{
+curl http://localhost/start/public/tdtinput/airports/airports_xml -XPUT -d '{
 	"name": "Airports", 
 	"occurence": 1,
 	"extract":{
@@ -11,7 +11,7 @@ curl http://localhost/test/public/tdtinput/airports/airports_xml -XPUT -d '{
 		"mapfile": "http://localhost/files/airport.xml.spec.ttl",
 		"datatank_package": "airports",
 		"datatank_resource": "airport_xml",
-		"datatank_uri": "http://localhost/test/public/"},
+		"datatank_uri": "http://localhost/start/public/"},
 	"load" : {
 		"type":"CLI"
 		}
