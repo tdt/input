d923d4ae42f5d8ea9d18f5eb0ee049e1a8971cdf
Michiel Vancoillie <info@ninetynine.be>
2013-09-29 23:55:08 +0200

    Lionel richie

--- src/tdt/input/HelloController.php
+++ src/tdt/input/HelloController.php
@@ -2,10 +2,11 @@
 
 namespace tdt\input;
 
-class HelloController{
+class HelloController extends \Controller{
 
     public function sayHello(){
 
-        return "Hello";
+        return "<html><body><img src='http://files.broadsheet.ie/wp-content/uploads/2012/01/Lionel_Richie_Hello_Lost_Sign.jpg' /></body></html>";
+
     }
 }
